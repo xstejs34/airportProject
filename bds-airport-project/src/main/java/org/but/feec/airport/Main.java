e7909c6fa3be876d3a1d5934e3ff78a8dc68b78c
xstejs34 <xstejs34@vutbr.cz>
2021-12-03 11:54:08 +0100

    test string

--- bds-airport-project/src/main/java/org/but/feec/airport/Main.java
+++ bds-airport-project/src/main/java/org/but/feec/airport/Main.java
@@ -3,5 +3,6 @@ package org.but.feec.airport;
 public class Main {
     public static void main(String[] args) {
         App.main(args);
+        System.out.print("Is this really necessary ?")
     }
 }
